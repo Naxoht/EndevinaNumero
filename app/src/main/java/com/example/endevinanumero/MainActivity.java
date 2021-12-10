5bc644fd242a74a366bcb8031ea2ca18b32a67e1
Naxoht <nhidalgotoca@gmail.com>
2021-12-10 12:04:27 +0100

    Imatge al ranking

--- app/src/main/java/com/example/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.endevinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.AlertDialog;
@@ -8,89 +9,161 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText number;
-    int tries = 1;
+    int tries;
     Random rand = new Random();
+    Intent intent;
+    String userName;
+    Context context = MainActivity.this;
+    int duration = Toast.LENGTH_SHORT;
+    CharSequence text = "";
     int randomNumber = rand.nextInt(100)+1;
+    File image = null;
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tries = 0;
         number = (EditText) findViewById(R.id.numUser);
         final Button button = findViewById(R.id.comprova);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                try {
+                    String numberUser = number.getText().toString();
+                    int value = Integer.parseInt(numberUser);
 
-                String numberUser = number.getText().toString();
-                int value = Integer.parseInt(numberUser);
-                Context context = MainActivity.this;
-                CharSequence text = "";
+                    if ( value > 100 || value < 1 ) {
+                        text = "El numero tiene que estar entre 1 y 100";
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }else {
+                        if (randomNumber > value) {
+                            text = "El numero que buscas es mayor al insertado.";
+                            tries++;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                            number.setText("");
+                        }
+                        if (randomNumber < value) {
+                            text = "El numero que buscas es menor al insertado.";
+                            tries++;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                            number.setText("");
+                        }
+                        if (randomNumber == value) {
+                            tries++;
+                            intent = new Intent(context,RankingActivity.class);
 
-                AlertDialog.Builder alert = new AlertDialog.Builder(context);
-
-                alert.setTitle("Felicidades!");
-                alert.setMessage("Has acertado el numero\nIngresa tu nombre para el ranking");
-
-                final EditText input = new EditText(context);
-                alert.setView(input);
-
-                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String userName = input.getText().toString();
-                        Intent intent = new Intent(context,RankingActivity.class);
-                        intent.putExtra("intentos",tries);
-                        intent.putExtra("user",userName);
-                        tries = 1;
-                        randomNumber = rand.nextInt(100)+1;
-                        startActivity(intent);
-
-
+                            callDialog(intent);
+                            randomNumber = rand.nextInt(100)+1;
+                        }
                     }
-                });
-
-                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        tries = 1;
-                        randomNumber = rand.nextInt(100)+1;
-                    }
-                });
-
-                int duration = Toast.LENGTH_SHORT;
-
-                if ( randomNumber > value) {
-                    text = "El numero que buscas es mayor al insertado.";
-                    tries++;
+                }catch (Exception e){
+                    text = "El campo del numero no puede estar vacio";
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
-                    number.setText("");
-                }if ( randomNumber < value) {
-                    text = "El numero que buscas es menor al insertado.";
-                    tries++;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                    number.setText("");
-                }if (randomNumber == value){
-                    alert.show();
+
                 }
-
             }
         });
     }
 
+    private void callDialog(Intent intent){
 
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
 
+        alert.setTitle("Felicidades!");
+        alert.setMessage("Has acertado el numero en "+tries+" intentos\nIngresa tu nombre para el ranking");
+
+        final EditText input = new EditText(context);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                userName = input.getText().toString();
+                if(!userName.isEmpty()){
+                    try {
+                        camera();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+                    Toast.makeText(context,"No se puede elegir un nombre vacio",duration).show();
+                    callDialog(intent);
+                }
+//                intent.putExtra("intentos",tries);
+//                intent.putExtra("user",userName);
+//                tries = 1;
+//                randomNumber = rand.nextInt(100)+1;
+//                startActivity(intent);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                tries = 0;
+                randomNumber = rand.nextInt(100)+1;
+            }
+        });
+        alert.show();
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        }
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        String currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void camera() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = createImageFile();
+
+        Uri photoURI = FileProvider.getUriForFile(this,
+                "com.endevinaNumero.android.fileprovider",
+                photoFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(takePictureIntent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        intent.putExtra("intentos",(tries));
+        intent.putExtra("user",userName);
+        intent.putExtra("image",image.toString());
+        tries = 0;
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/com/example/endevinanumero/RankingActivity.java
+++ app/src/main/java/com/example/endevinanumero/RankingActivity.java
@@ -2,28 +2,37 @@ package com.example.endevinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class RankingActivity extends AppCompatActivity {
-    static List<String> usersTries = new LinkedList<String>();
-    static List<String> users = new LinkedList<String>();
+    static ArrayList<Integer> usersTries = new ArrayList<Integer>();
+    static ArrayList<String> users = new ArrayList<String>();
+    static ArrayList<Uri> userImage = new ArrayList<Uri>();
     // Model: Record (intents=puntuació, nom)
     class Record {
         public int intents;
         public String nom;
+        public Uri foto;
 
-        public Record(int _intents, String _nom ) {
+        public Record(String _nom, Uri uri,int _intents ) {
             intents = _intents;
             nom = _nom;
+            foto = uri;
+
         }
     }
     // Model = Taula de records: utilitzem ArrayList
@@ -37,12 +46,32 @@ public class RankingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         users.add(getIntent().getStringExtra("user"));
-        usersTries.add(Integer.toString(getIntent().getIntExtra("intentos",1)));
+        usersTries.add((getIntent().getIntExtra("intentos",1)));
+        userImage.add(Uri.fromFile(new File(getIntent().getStringExtra("image"))));
+//        int i, j, auxTries;
+//        for (i = 0; i < usersTries.size() - 1; i++) {
+//            for (j = 0; j < usersTries.size()- 1; j++) {
+//                if (usersTries.get(j + 1) < usersTries.get(j)) {
+//                    auxTries = usersTries.get(j + 1);
+//                    usersTries.set(j + 1,usersTries.get(j));
+//                    System.out.println(j);
+//                    usersTries.set(j,usersTries.get(auxTries));
+//
+//                    String auxUsers = users.get(j + 1);
+//                    users.set(j + 1,users.get(j));
+//                    users.set(j,users.get(auxTries));
+//
+//                    Uri auxUri = userImage.get(j + 1);
+//                    userImage.set(j + 1,userImage.get(j));
+//                    userImage.set(j,userImage.get(auxTries));
+//                }
+//            }
+//        }
         // Inicialitzem model
         records = new ArrayList<Record>();
         // Afegim alguns exemples
-        for(int i = 0; i < users.size() ; i++){
-            records.add(new Record(Integer.parseInt(usersTries.get(i)),users.get(i)));
+        for(int z = 0; z < users.size() ; z++){
+            records.add(new Record(users.get(z),userImage.get(z),usersTries.get(z)));
         }
 
 
@@ -60,6 +89,8 @@ public class RankingActivity extends AppCompatActivity {
                 // "Pintem" valors (també quan es refresca)
                 ((TextView) convertView.findViewById(R.id.nom)).setText(getItem(pos).nom);
                 ((TextView) convertView.findViewById(R.id.intents)).setText(Integer.toString(getItem(pos).intents));
+                ((ImageView) convertView.findViewById(R.id.imageView)).setImageURI(getItem(pos).foto);
+
                 return convertView;
             }
 
@@ -68,6 +99,11 @@ public class RankingActivity extends AppCompatActivity {
         // busquem la ListView i li endollem el ArrayAdapter
         ListView lv = (ListView) findViewById(R.id.recordsView);
         lv.setAdapter(adapter);
+    }
+    protected File getFile(){
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photo = new File(path,"imatge.jpg");
+        return photo;
     }
 
 }
